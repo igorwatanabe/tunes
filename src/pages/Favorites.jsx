6a488de58012b9244389f05909dc8f179b3df7e2
Igor <eng.igorwatanabe@gmail.com>
2023-04-14 13:38:26 -0300

    Item 1 finalizado

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Favorites extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        A
+      </div>
+    );
+  }
+}
+export default Favorites;

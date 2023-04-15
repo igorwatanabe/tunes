3cfe19e38280c0ef8b25665af6b77597324a7f29
Igor <eng.igorwatanabe@gmail.com>
2023-04-15 00:44:17 -0300

    Item 2 finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,103 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  state = {
+    loginName: '',
+    buttonDisabled: true,
+    isLoading: false,
+  };
+
+  // componentDidMount() {
+  //   console.log('componentDidMount');
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   return true;
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log('componentDidUpdate');
+  // }
+
+  validation = () => {
+    const {
+      loginName,
+    } = this.state;
+
+    const numberThree = 3;
+    const minLengthName = loginName.length >= numberThree;
+
+    this.setState({
+      buttonDisabled: !(
+        minLengthName
+      ),
+    });
+  };
+
+  onInputChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, this.validation);
+  };
+
+  onClickEntrar = async (nomeDigitado) => {
+    const { history } = this.props;
+    console.log(history);
+    this.setState({
+      isLoading: true, // altera no state, para aparecer o Carregando...
+    }, async () => {
+      await createUser(nomeDigitado);
+      this.setState({
+        isLoading: false,
+      }, () => history.push('/search')); // history.push REDIRECIONA
+    });
+  };
+
   render() {
+    const {
+      buttonDisabled,
+      loginName,
+      isLoading,
+    } = this.state;
+
     return (
       <div data-testid="page-login">
-        F
+        {(isLoading) ? <h2><Loading /></h2>
+          : (
+            <form>
+              <input
+                type="text"
+                name="loginName"
+                value={ loginName }
+                data-testid="login-name-input"
+                placeholder="Nome"
+                onChange={ this.onInputChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                disabled={ buttonDisabled }
+                onClick={ () => this.onClickEntrar({ name: loginName }) }
+              >
+                Entrar
+              </button>
+            </form>)}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
 export default Login;

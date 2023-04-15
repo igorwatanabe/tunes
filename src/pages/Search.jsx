aadd2e31566f3a289017caab7056353ca1406764
Igor <eng.igorwatanabe@gmail.com>
2023-04-15 11:57:41 -0300

    Item 5 finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
 
 class Search extends React.Component {
+  state = {
+    searchArtist: '',
+    buttonSearchDisabled: true,
+  };
+
+  validation = () => {
+    const {
+      searchArtist,
+    } = this.state;
+
+    const numberTwo = 2;
+    const minLengthName = searchArtist.length >= numberTwo;
+
+    this.setState({
+      buttonSearchDisabled: !(
+        minLengthName
+      ),
+    });
+  };
+
+  onInputChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, this.validation);
+  };
+
   render() {
+    const {
+      searchArtist,
+      buttonSearchDisabled,
+    } = this.state;
     return (
       <div data-testid="page-search">
-        Deu certo
+        <form>
+          <input
+            type="text"
+            name="searchArtist"
+            value={ searchArtist }
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            onChange={ this.onInputChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ buttonSearchDisabled }
+            // onClick={ }
+          >
+            Procurar
+          </button>
+        </form>
       </div>
     );
   }

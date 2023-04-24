5ebc7e95a909e6de06bd4d0e7c54103d57a083fc
Igor <eng.igorwatanabe@gmail.com>
2023-04-24 09:31:08 -0300

    item 13 finalizado

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    isLoading: false,
+    userName: '',
+    userEmail: '',
+    userDescription: '',
+    userImg: '',
+  };
+
+  componentDidMount() {
+    this.renderUser();
+  }
+
+  renderUser = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const user = await getUser();
+    this.setState({
+      isLoading: false,
+      userName: user.name,
+      userEmail: user.email,
+      userDescription: user.description,
+      userImg: user.image,
+    });
+    console.log(user);
+  };
+
   render() {
+    const {
+      isLoading,
+      userName,
+      userEmail,
+      userDescription,
+      userImg,
+    } = this.state;
     return (
       <div data-testid="page-profile">
-        B
+        { isLoading ? <Loading />
+          : (
+            <>
+              <p>{userName}</p>
+              <p>{userEmail}</p>
+              <p>{userDescription}</p>
+              <img data-testid="profile-image" src={ userImg } alt="imagem do usuário" />
+              <Link to="/profile/edit">
+                <button>Editar perfil</button>
+              </Link>
+            </>
+          )}
       </div>
     );
   }
